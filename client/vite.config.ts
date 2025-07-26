4ab59cc597bbcb3074d0d10c2adb597e9658f8ae
Sushil Rahatole <sushilrahatole@gmail.com>
2025-07-26 18:43:46 +0530

    Final Changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,17 +3,14 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  root: "client",
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client/src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   build: {
-    outDir: "../dist", // Output to project-root/dist
+    outDir: "dist",
     emptyOutDir: true,
   },
 });
